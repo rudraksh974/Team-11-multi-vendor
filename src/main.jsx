f87252ad0282bacf05792b51fbd643dc3f678e11
sankalp-singh-07 <sankalp.singh.somvanshi@gmail.com>
2025-12-04 12:58:47 +0530

    App.jsx, main.jsx and index.css changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
-import { CartProvider } from "./context/CartContext";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import {BrowserRouter} from 'react-router-dom'
+import StoreContextProvider from './components/context/StoreContext.jsx'
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </BrowserRouter>
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+  <StoreContextProvider>
+    <App />
+  </StoreContextProvider>
+  </BrowserRouter>,
+)
